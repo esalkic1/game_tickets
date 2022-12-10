2b9c93fa61affa3071fbaeec443e59975de6ca3b
Emir Salkić <esalkic1@etf.unsa.ba>
2022-12-10 23:16:28 +0100

    Domain model for game tickets app

--- src/main/java/ba/unsa/etf/rpr/domain/Ticket.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Ticket.java
@@ -6,4 +6,44 @@ public class Ticket {
     private Customer customer;
     private int price;
     private String stand;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getStand() {
+        return stand;
+    }
+
+    public void setStand(String stand) {
+        this.stand = stand;
+    }
 }
