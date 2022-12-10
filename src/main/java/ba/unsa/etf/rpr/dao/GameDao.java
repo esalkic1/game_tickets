5c763cf5ddab47d002040e5e8fc10a3317a5c74d
Emir Salkić <esalkic1@etf.unsa.ba>
2022-12-11 00:41:07 +0100

    GameDao interface

--- src/main/java/ba/unsa/etf/rpr/dao/GameDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/GameDao.java
@@ -2,6 +2,7 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Game;
 
+import java.util.Date;
 import java.util.List;
 
 public interface GameDao extends Dao<Game>{
@@ -9,8 +10,15 @@ public interface GameDao extends Dao<Game>{
     /**
      * Returns the list of games against the given opponent
      * @param text name of the opponent team
-     * @return list of opponents
+     * @return list of games
      */
     List<Game> searchByOpponent(String text);
 
+    /**
+     * Finds games in the given period of time
+     * @param start starting date
+     * @param end ending date
+     * @return list of games
+     */
+    List<Game> getByDateRange(Date start, Date end);
 }
