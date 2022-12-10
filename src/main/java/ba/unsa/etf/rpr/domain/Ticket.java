bd3eb6aa41825a75123cfafc84844424166d1c32
Emir Salkić <esalkic1@etf.unsa.ba>
2022-12-10 23:15:49 +0100

    Domain model for game tickets app

---
+++ src/main/java/ba/unsa/etf/rpr/domain/Ticket.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr.domain;
+
+public class Ticket {
+    private int id;
+    private Game game;
+    private Customer customer;
+    private int price;
+    private String stand;
+}
