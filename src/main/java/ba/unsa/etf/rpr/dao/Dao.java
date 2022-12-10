1981e401ac363848b2814f8bb7c72345b91e9457
Emir Salkić <esalkic1@etf.unsa.ba>
2022-12-10 23:29:34 +0100

    Dao interface

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -1,9 +1,31 @@
 package ba.unsa.etf.rpr.dao;
 
+import java.util.List;
+
 /**
  * interface for dao classes
  * @author Emir Salkic
  */
 public interface Dao<T> {
 
+    /**
+     * Method that gets an entity by received id
+     * @param id primary key
+     * @return entity from database
+     */
+    T getById(int id);
+
+    /**
+     * Method that adds the item as entity in the database
+     * @param item to be added in the database
+     * @return added item
+     */
+    T add(T item);
+
+
+    T update(T item);
+
+    void delete(int id);
+
+    List<T> getAll();
 }
