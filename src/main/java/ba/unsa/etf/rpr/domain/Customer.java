80468c9d3dd88dc9d481b4b80fe414dfb8670380
Emir Salkić <esalkic1@etf.unsa.ba>
2022-12-10 19:58:41 +0100

    Customer class

--- src/main/java/ba/unsa/etf/rpr/domain/Customer.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Customer.java
@@ -1,4 +1,47 @@
 package ba.unsa.etf.rpr.domain;
 
+/**
+ * Ticket customer information
+ *
+ * @author Emir Salkic
+ */
 public class Customer {
+    private int id;
+    private String name;
+    private String surname;
+    private int numberOfTickets;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getNumberOfTickets() {
+        return numberOfTickets;
+    }
+
+    public void setNumberOfTickets(int numberOfTickets) {
+        this.numberOfTickets = numberOfTickets;
+    }
+
+
 }
