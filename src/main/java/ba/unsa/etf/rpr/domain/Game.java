71fd9490de522f88a7525820a5d3d6cb4dc745bd
Emir Salkić <esalkic1@etf.unsa.ba>
2022-12-10 23:10:18 +0100

    Game class

--- src/main/java/ba/unsa/etf/rpr/domain/Game.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Game.java
@@ -1,5 +1,59 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Date;
+
+/**
+ * Game information
+ * @author Emir Salkic
+ */
 public class Game {
+    private int id;
+    private int capacity;
+    private int sold;
+    private String opponent;
+    private Date date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getSold() {
+        return sold;
+    }
+
+    public void setSold(int sold) {
+        this.sold = sold;
+    }
+
+    public String getOpponent() {
+        return opponent;
+    }
+
+    public void setOpponent(String opponent) {
+        this.opponent = opponent;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
 
 }
+
+
