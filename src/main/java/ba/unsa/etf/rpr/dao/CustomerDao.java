53f39df498a73806ac53adae6b6733457d20b974
Emir Salkić <esalkic1@etf.unsa.ba>
2022-12-10 23:57:24 +0100

    CustomerDao interface

---
+++ src/main/java/ba/unsa/etf/rpr/dao/CustomerDao.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Customer;
+
+import java.util.List;
+
+/**
+ * Dao interface for Customer class
+ * @author Emir Salkic
+ */
+
+public interface CustomerDao extends Dao<Customer>{
+
+    /**
+     * Method that finds all customers with a given name
+     *
+     * @param text name of the searched customer
+     * @return list of customers with the given name
+     */
+    List<Customer> searchByName(String text);
+
+    /**
+     * Method that finds all customers with a given surname
+     * @param text surname of the searched customer
+     * @return list of customers with the given surname
+     */
+    List<Customer> searchBySurname(String text);
+}
