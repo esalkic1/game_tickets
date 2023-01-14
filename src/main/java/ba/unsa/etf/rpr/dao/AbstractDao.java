456df6b5272830df894073d900009936be597ea2
Emir Salkić <esalkic1@etf.unsa.ba>
2023-01-14 20:08:14 +0100

    Methods implemented

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -1,6 +1,8 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Idable;
+import ba.unsa.etf.rpr.exceptions.TicketException;
+import com.sun.scenario.effect.impl.prism.PrReflectionPeer;
 
 import java.io.IOException;
 import java.sql.*;
@@ -24,4 +26,50 @@ public abstract class AbstractDao<T extends Idable> implements  Dao<T>{
             e.printStackTrace();
         }
     }
+
+    public Connection getConnection() {
+        return this.connection;
+    }
+
+    public abstract T row2object(ResultSet rs) throws TicketException;
+
+    public abstract Map<String, Object> object2row(T object);
+
+    public T getById(int id) throws TicketException{
+        String query = "SELECT * FROM "+this.tableName+" WHERE id = ?";
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) { // result set is iterator.
+                T result = row2object(rs);
+                rs.close();
+                return result;
+            } else {
+                throw new TicketException("Object not found");
+            }
+        }
+        catch (SQLException e) {
+            throw new TicketException(e.getMessage(), e);
+        }
+    }
+
+    public List<T> getAll() throws TicketException{
+        String query = "SELECT * FROM "+ tableName;
+        List<T> results = new ArrayList<T>();
+        try{
+            PreparedStatement stmt = getConnection().prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()){ // result set is iterator.
+                T object = row2object(rs);
+                results.add(object);
+            }
+            rs.close();
+            return results;
+        }catch (SQLException e){
+            throw new TicketException(e.getMessage(), e);
+        }
+    }
+
+
 }
--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.exceptions.TicketException;
+
 import java.util.List;
 
 /**
@@ -13,31 +15,31 @@ public interface Dao<T> {
      * @param id primary key
      * @return entity from database
      */
-    T getById(int id);
+    T getById(int id) throws TicketException;
 
     /**
      * Method that adds the item as entity in the database
      * @param item to be added in the database
      * @return added item
      */
-    T add(T item);
+    T add(T item) throws TicketException;
 
     /**
      * Method that updates existing entity in database
      * @param item to be updated
      * @return updated version of entity
      */
-    T update(T item);
+    T update(T item) throws TicketException;
 
     /**
      * Method that deletes an entity from database based on id
      * @param id primary key of the to be deleted entity
      */
-    void delete(int id);
+    void delete(int id) throws TicketException;
 
     /**
      * Method that returns all the items stored in the database
      * @return returns all items as a list
      */
-    List<T> getAll();
+    List<T> getAll() throws TicketException;
 }
