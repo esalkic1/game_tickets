16f7d922c81ff5668ebbb534254fe0f17d4419c8
Emir Salkić <esalkic1@etf.unsa.ba>
2023-01-24 23:36:45 +0100

    controllers for new fxmls

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/UserMainController.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.controllers;
+
+import ba.unsa.etf.rpr.domain.Customer;
+
+public class UserMainController {
+
+    private Customer customer;
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
