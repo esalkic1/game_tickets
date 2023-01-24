848eb32dec2f050303d92b7dbf85fe780498bf2f
Emir Salkić <esalkic1@etf.unsa.ba>
2023-01-24 21:28:14 +0100

    Login button click made

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -1,14 +1,28 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HomeController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    public TextField tfUsername;
+    public PasswordField tfPassword;
+    public Button btnLogin;
+    public Button btnRegister;
+
+    public void loginBtnClick(ActionEvent actionEvent) {
+        if(tfUsername.getText().isEmpty()){
+            Alert empty = new Alert(Alert.AlertType.ERROR);
+            empty.setTitle("Error");
+            empty.setHeaderText("Niste unijeli korisnicko ime!");
+            empty.setContentText("Pokušajte ponovo");
+
+            empty.showAndWait();
+        }
+    }
+
+    public void registerBtnClick(ActionEvent actionEvent) {
     }
 }
