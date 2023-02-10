cd12050a0b1dc3e86a3181f33c3ec42b18c2d15e
Emir Salkić <esalkic1@etf.unsa.ba>
2023-02-10 11:29:04 +0100

    AdminEdit choicebox now working

--- src/main/java/ba/unsa/etf/rpr/controllers/AdminEditController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AdminEditController.java
@@ -1,4 +1,19 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+
 public class AdminEditController {
+    public ChoiceBox cbCompetition;
+    public Button btnAddGame;
+    public Button btnDeleteGame;
+
+    public void initialize(){
+        cbCompetition.getItems().add("Premijer Liga BiH");
+        cbCompetition.getItems().add("Kup BiH");
+        cbCompetition.getItems().add("Liga Prvaka");
+        cbCompetition.getItems().add("Europa Liga");
+        cbCompetition.getItems().add("Liga konferencija");
+        cbCompetition.getItems().add("Prijateljska utakmica");
+    }
 }
