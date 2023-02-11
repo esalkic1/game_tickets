46141667361c7c1594655a55cdcf31a9ab714a3c
Emir Salkić <esalkic1@etf.unsa.ba>
2023-02-11 11:17:29 +0100

    Getting data from last stage

--- src/main/java/ba/unsa/etf/rpr/controllers/PurchaseConfirmController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PurchaseConfirmController.java
@@ -1,11 +1,22 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.domain.Customer;
+import ba.unsa.etf.rpr.domain.Game;
 import ba.unsa.etf.rpr.exceptions.TicketException;
 import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 
 public class PurchaseConfirmController {
     public ChoiceBox cbStand;
+    public Button btnConfirmBuy;
+    public Button btnCancel;
+
+    public PurchaseConfirmController(Customer cust, Game game) {
+        System.out.println(cust.getPassword());
+        System.out.println(game.getOpponent());
+    }
 
     public void initialize(){
         cbStand.getItems().add("Jug");
@@ -13,5 +24,11 @@ public class PurchaseConfirmController {
         cbStand.getItems().add("Sjever");
         cbStand.getItems().add("Zapad");
         cbStand.getItems().add("Zapad-VIP");
+    }
+
+    public void CancelBtnClick(ActionEvent actionEvent) {
+    }
+
+    public void ConfirmBtnClick(ActionEvent actionEvent) {
     }
 }
