ff47fe9fb4a371a22ef19efeeebfb05a56d22950
Emir Salkić <esalkic1@etf.unsa.ba>
2023-02-11 22:30:52 +0100

    OnAction added

--- src/main/java/ba/unsa/etf/rpr/controllers/EditProfileController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/EditProfileController.java
@@ -1,11 +1,17 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.dao.AbstractDao;
 import ba.unsa.etf.rpr.domain.Customer;
+import javafx.event.ActionEvent;
 
 public class EditProfileController {
 
     Customer customer;
     public EditProfileController(Customer cust){
         this.customer = cust;
+    }
+
+    public void EditCustomerBtnClick(ActionEvent actionEvent){
+
     }
 }
