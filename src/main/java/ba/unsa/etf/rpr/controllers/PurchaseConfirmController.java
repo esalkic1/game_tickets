d4d9ffa7d938ea161c92414bcfef01e737db618e
Emir Salkić <esalkic1@etf.unsa.ba>
2023-02-11 10:40:39 +0100

    Choicebox implemented

--- src/main/java/ba/unsa/etf/rpr/controllers/PurchaseConfirmController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/PurchaseConfirmController.java
@@ -1,4 +1,17 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.exceptions.TicketException;
+import javafx.collections.FXCollections;
+import javafx.scene.control.ChoiceBox;
+
 public class PurchaseConfirmController {
+    public ChoiceBox cbStand;
+
+    public void initialize(){
+        cbStand.getItems().add("Jug");
+        cbStand.getItems().add("Istok");
+        cbStand.getItems().add("Sjever");
+        cbStand.getItems().add("Zapad");
+        cbStand.getItems().add("Zapad-VIP");
+    }
 }
