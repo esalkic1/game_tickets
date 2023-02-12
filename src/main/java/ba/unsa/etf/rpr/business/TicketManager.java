ce687ae34f3dbd2d059b2f5c006c6d05db975dc0
Emir Salkić <esalkic1@etf.unsa.ba>
2023-02-12 09:00:18 +0100

    Ticket Manager implemented

--- src/main/java/ba/unsa/etf/rpr/business/TicketManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/TicketManager.java
@@ -1,4 +1,35 @@
 package ba.unsa.etf.rpr.business;
 
+
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Ticket;
+import ba.unsa.etf.rpr.exceptions.TicketException;
+
+import java.util.List;
+
 public class TicketManager {
+
+    public List<Ticket> getByPriceRange(int min, int max) throws TicketException {
+        return DaoFactory.ticketDao().getByPriceRange(min, max);
+    }
+
+    public Ticket getById(int id) throws TicketException {
+        return DaoFactory.ticketDao().getById(id);
+    }
+
+    public Ticket add(Ticket ticket) throws TicketException{
+        return DaoFactory.ticketDao().add(ticket);
+    }
+
+    public void update(Ticket ticket) throws TicketException{
+        DaoFactory.ticketDao().update(ticket);
+    }
+
+    public void delete(int id) throws TicketException{
+        DaoFactory.ticketDao().delete(id);
+    }
+
+    public List<Ticket> getAll() throws TicketException{
+        return DaoFactory.ticketDao().getAll();
+    }
 }
